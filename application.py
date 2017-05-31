3a89b8321374116d5f1ecc9ebbab3b125771b523
Oliver Stenbom <oliver@stenbom.eu>
2017-05-31 17:52:40 +0100

    running application on main

--- application.py
+++ application.py
@@ -9,3 +9,9 @@ application.secret_key = 'development key'
 
 import views
 import forms
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
