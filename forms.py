404b356aea425e139d66b58a1b8e2aa28161b2e0
Oliver Stenbom <oliver@stenbom.eu>
2017-05-31 18:23:09 +0100

    Regex fix

--- forms.py
+++ forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired, Regexp
 
 class FindVoterForm(Form):
     firstname = StringField('firstname', validators=[DataRequired("No name entered."),
-    Regexp('[A-z\'- ]+', message="Invalid first name entered")])
+    Regexp('[A-Za-z\'-]+', message="Invalid first name entered")])
     postcode = StringField('password', validators=[DataRequired("No postcode entered."),
     Regexp('(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})', message="Invalid postcode entered")])
