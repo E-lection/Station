b1e6014e38c1ad0f4d26bc4a9ec8ab562e03994b
Oliver Stenbom <oliver@stenbom.eu>
2017-05-31 18:17:02 +0100

    Form back to forms.py, views permanently in application main, regex fixing

--- application.py
+++ application.py
@@ -2,32 +2,20 @@
 # $ python -m flask run
 
 from flask import Flask
+from flask import render_template
+from forms import FindVoterForm
+
 application = Flask(__name__)
 
 application.config['TEMPLATES_AUTO_RELOAD'] = True
 application.secret_key = 'development key'
 
-from flask_wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired, Regexp
-from flask import render_template
-
-class FindVoterForm(Form):
-    firstname = StringField('firstname', validators=[DataRequired("No name entered."),
-    Regexp('/^[a-z ,.\'-]+$/i', message="Invalid first name entered")])
-    postcode = StringField('password', validators=[DataRequired("No postcode entered."),
-    Regexp('(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})', message="Invalid postcode entered")])
-
 @application.route('/')
-def index():
-    return render_template('index.html')
-
-@application.route('/station')
 def station():
     form = FindVoterForm()
     return render_template('station.html', form=form)
 
-@application.route('/station', methods=['POST'])
+@application.route('/', methods=['POST'])
 def find_voter():
     form = FindVoterForm()
     if form.validate_on_submit():
