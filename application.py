13e63ccea40df865afade52b4cc6bb5c843ff3a2
Oliver Stenbom <oliver@stenbom.eu>
2017-05-31 17:41:50 +0100

    Renamed app -> application

--- application.py
+++ application.py
@@ -2,10 +2,10 @@
 # $ python -m flask run
 
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)
 
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.secret_key = 'development key'
+application.config['TEMPLATES_AUTO_RELOAD'] = True
+application.secret_key = 'development key'
 
 import views
 import forms
--- views.py
+++ views.py
@@ -1,19 +1,19 @@
-from application import app
+from application import application
 from flask import render_template
 
 from forms import FindVoterForm
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/station')
+@application.route('/station')
 def station():
     form = FindVoterForm()
     return render_template('station.html', form=form)
 
-@app.route('/station', methods=['POST'])
+@application.route('/station', methods=['POST'])
 def find_voter():
     form = FindVoterForm()
     if form.validate_on_submit():
