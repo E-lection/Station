9262a298c1d981d3535173c3af49824135bc47b0
Jennifer Lea <jennifer.lea15@imperial.ac.uk>
2017-05-31 14:24:55 +0100

    Now redirects to voter database page on success.

--- e_lection/views.py
+++ e_lection/views.py
@@ -17,5 +17,5 @@ def station():
 def find_voter():
     form = FindVoterForm()
     if form.validate_on_submit():
-        return render_template('index.html', form = form)
+        return render_template('voterdb.html', form = form)
     return render_template('station.html', form = form)
