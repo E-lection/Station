5d842071190e01e897f30f23ad95519a5ae3d4af
Jennifer Lea <jennifer.lea15@imperial.ac.uk>
2017-05-31 13:40:11 +0100

    Naive implementation of posting first name from the form.

--- e_lection/views.py
+++ e_lection/views.py
@@ -1,5 +1,6 @@
 from e_lection import app
 from flask import render_template
+from flask import request
 
 @app.route('/')
 def index():
@@ -9,3 +10,9 @@ def index():
 @app.route('/station')
 def station():
     return render_template('station.html')
+
+@app.route('/station', methods=['POST'])
+def my_form_post():
+    firstname = request.form['firstname']
+    postcode = request.form['postcode']
+    return firstname
