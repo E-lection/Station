6142bdf93bde3f08a241168578f755c46df51c2d
Rishabh Jain <wakeuprj@gmail.com>
2017-06-01 15:33:31 +0100

    Request response from voter database.

--- application.py
+++ application.py
@@ -4,6 +4,8 @@
 from flask import Flask
 from flask import render_template
 from forms import FindVoterForm
+from flask import request
+import urllib, urllib2
 
 # Configuration. Example cases for TESTING
 FIRSTNAME = 'Jenny'
@@ -16,18 +18,28 @@ application.config.from_object(__name__)
 
 @application.route('/')
 def station():
-    form = FindVoterForm()
+    form = FindVoterForm(request.form)
     return render_template('station.html', form=form)
 
 @application.route('/', methods=['POST'])
 def find_voter():
-    form = FindVoterForm()
+    form = FindVoterForm(request.form)
     if form.validate_on_submit():
+        firstname = request.form['firstname']
+        postcode = request.form['postcode']
+        url = createSearchURL(firstname, postcode)
+        dbresult = urllib2.urlopen(url).read()
         return render_template('voterdb.html', form=form)
     return render_template('station.html', form=form)
-
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = True
     application.run()
+
+
+def createSearchURL(firstname, postcode):
+    firstname = "/voter_name/" + urllib.quote(firstname)
+    postcode = "/postcode/" + urllib.quote(postcode)
+    url = "http://voting.eelection.co.uk/get_voters/station_id/1"+firstname+postcode
+    return url
