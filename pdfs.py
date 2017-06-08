19ba04df0e293edc709bd5513acbece8bdb54c28
Jennifer Lea <jennifer.lea15@imperial.ac.uk>
2017-06-08 12:34:32 +0100

    Added pdfs python file to generate the pdfs.

---
+++ pdfs.py
@@ -0,0 +1,21 @@
+from xhtml2pdf import pisa
+from StringIO import StringIO
+from flask import render_template
+
+def create_pdf(filename, voter_pin):
+    # open output file for writing (truncated binary)
+    #resultFile = open(outputFilename, "w+b")
+
+    # # convert HTML to PDF
+    # pisaStatus = pisa.CreatePDF(
+    #         sourceHtml,                # the HTML to convert
+    #         dest=resultFile)           # file handle to recieve result
+    #
+    # # close output file
+    # resultFile.close()                 # close output file
+    pdf = pisa.CreatePDF(
+        render_template('voterpincard.html'),
+        file(filename, "wb")
+        )
+    # return True on success and False on errors
+    return pdf
