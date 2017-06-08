6182815884e24eb370024e0bae2c51ea74e097e8
Jennifer Lea <jennifer.lea15@imperial.ac.uk>
2017-06-08 14:06:31 +0100

    Pushing tests to master?

--- station_tests.py
+++ station_tests.py
@@ -8,15 +8,16 @@ import create_user
 
 TEST_USERNAME = 'station_test'
 TEST_PASSWORD = 'test_secret'
+BAD_LOGIN = 'bad'
 
 class StationTestCase(unittest.TestCase):
 
     def setUp(self):
         application.application.config['TESTING'] = True
         self.application = application.application.test_client()
-        create_user.create_user('station_test', 'test_secret', 1)
+        create_user.create_user(TEST_USERNAME, TEST_PASSWORD, 1)
     def tearDown(self):
-        db.deleteUser('station_test')
+        db.deleteUser(TEST_USERNAME)
 
     # Find voter helper function
     def find_voter(self, firstname, postcode):
@@ -54,12 +55,17 @@ class StationTestCase(unittest.TestCase):
         result = self.application.get('/login')
         assert b'Username' and b'Password' in result.data
 
-    # Test that you can log in with an incorrect username
-    # and password combination
+    # Test that logging in with an incorrect username
+    # and password combination asks you again
     def test_incorrect_login(self):
-        result = self.login("bad", "bad")
-        print result
-        assert b'Username' and b'Password' in result.data
+        result = self.login(BAD_LOGIN, BAD_LOGIN)
+        assert b'Login unsuccessful' and b'Username' and b'Password' in result.data
+
+    # Test that logging in with correct credentials
+    # takes you to the search voter page
+    def test_correct_login(self):
+        result = self.login(BAD_LOGIN, BAD_LOGIN)
+        assert b'First name(s)' and b'Postcode' in result.data
 
     # Tests that it asks for a first name
     def test_firstname_field_loaded(self):
