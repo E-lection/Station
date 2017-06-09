7ace14a447b87c60a2ff5ceed1b2975a7610f7dc
Jennifer Lea <jennifer.lea15@imperial.ac.uk>
2017-06-09 14:05:06 +0100

    Puts the right pin in the pdf and serves it to the user.

--- pdfs.py
+++ pdfs.py
@@ -1,21 +1,56 @@
-from xhtml2pdf import pisa
-from StringIO import StringIO
-from flask import render_template
+from flask import Flask, Response
+import jinja2
+import os
+from jinja2 import Template
 
-def create_pdf(filename, voter_pin):
-    # open output file for writing (truncated binary)
-    #resultFile = open(outputFilename, "w+b")
+import pexpect
 
-    # # convert HTML to PDF
-    # pisaStatus = pisa.CreatePDF(
-    #         sourceHtml,                # the HTML to convert
-    #         dest=resultFile)           # file handle to recieve result
-    #
-    # # close output file
-    # resultFile.close()                 # close output file
-    pdf = pisa.CreatePDF(
-        render_template('voterpincard.html'),
-        file(filename, "wb")
-        )
-    # return True on success and False on errors
-    return pdf
+PDFLATEX_COMMAND = "pdflatex -interaction=nonstopmode -output-directory=tex {}"
+TEMPLATE_TEX = 'tex/voterpincard.tex'
+FILENAME = 'pin'
+VOTERPIN_TEX = 'tex/' + FILENAME + '.tex'
+VOTERPIN_PDF = 'tex/' + FILENAME + '.pdf'
+
+latex_jinja_env = jinja2.Environment(
+	block_start_string = '\BLOCK{',
+	block_end_string = '}',
+	variable_start_string = '\VAR{',
+	variable_end_string = '}',
+	comment_start_string = '\#{',
+	comment_end_string = '}',
+	line_statement_prefix = '%%',
+	line_comment_prefix = '%#',
+	trim_blocks = True,
+	autoescape = False,
+	loader = jinja2.FileSystemLoader(os.path.abspath('.'))
+)
+
+def create_pdf(voterpin):
+	# Get the latex template
+	template = latex_jinja_env.get_template(TEMPLATE_TEX)
+
+	# Render this with the correct voter pin
+	pin_tex = template.render(voterpin=voterpin)
+
+	# Write pin_tex to a file
+	file = open(VOTERPIN_TEX,'w')
+	file.write(pin_tex)
+	file.close()
+
+	# Render it as a pdf
+	tex_render = pexpect.spawn(PDFLATEX_COMMAND.format(VOTERPIN_TEX))
+	try:
+		tex_render.expect("Output written on", timeout=30)
+	except pexpect.TIMEOUT:
+		return "Rendering the PDF took too long, please try again!"
+
+    # Load PDF into memory
+	pdf_file = open(VOTERPIN_PDF)
+
+    # Remove files from filesystem
+	os.remove(VOTERPIN_TEX)
+	os.remove(VOTERPIN_PDF)
+	os.remove('tex/' + FILENAME + '.aux')
+	os.remove('tex/' + FILENAME + '.log')
+
+	return Response(pdf_file, mimetype='application/pdf')
