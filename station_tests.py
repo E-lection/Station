2f767ca0123a6a8b51662298a1c805a40f273b96
Jennifer Lea <jennifer.lea15@imperial.ac.uk>
2017-06-13 17:28:06 +0100

    Solved problem of not opening a PDF when the button says 'new pin'.

--- station_tests.py
+++ station_tests.py
@@ -82,14 +82,14 @@ class StationTestCase(unittest.TestCase):
         assert b'Error! Invalid postcode entered.' in result.data
 
     # Tests that it goes to the database view when a valid first
-    # name and postcode is entered
-    # def test_valid_voter_search(self):
-    #     self.login(TEST_USERNAME, TEST_PASSWORD)
-    #     voter_name = 'James'
-    #     voter_postcode = 'SW7 1BE'
-    #     self.find_voter(voter_name, voter_postcode)
-    #     print result.data
-    #     assert voter_name and voter_postcode and b'Request PIN' in result.data
+    name and postcode is entered
+    def test_valid_voter_search(self):
+        self.login(TEST_USERNAME, TEST_PASSWORD)
+        voter_name = 'Nora'
+        voter_postcode = 'IV1 1AA'
+        self.find_voter(voter_name, voter_postcode)
+        print result.data
+        assert voter_name and voter_postcode and b'Request PIN' in result.data
 
 if __name__ == '__main__':
     unittest.main()
